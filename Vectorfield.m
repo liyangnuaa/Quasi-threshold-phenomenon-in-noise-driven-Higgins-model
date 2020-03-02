f86be606ec25db6d62a67c20a06954f9cb861ec0
liyangnuaa <61436455+liyangnuaa@users.noreply.github.com>
2020-03-01 20:51:46 -0600

    Update and rename main.m to Vectorfield.m

--- Vectorfield.m
+++ Vectorfield.m
@@ -76,3 +76,19 @@ hold off
 % plot(xnode(1),xnode(2),'r+');
 
 
+
+function xout=rk4(t0,h,x0)
+k1=h*fun(t0,x0);
+k2=h*fun(t0+h/2,x0+0.5*k1);
+k3=h*fun(t0+h/2,x0+0.5*k2);
+k4=h*fun(t0+h,x0+k3);
+xout=x0+(k1+2*k2+2*k3+k4)/6;
+
+
+function y=fun(~,x)
+
+global p q
+
+y=zeros(size(x));
+y(1,:)=1-x(1,:).*x(2,:);
+y(2,:)=p*x(2,:).*(x(1,:)-(1+q)./(q+x(2,:)));
